38da54819046f6b7879b0c80e8281f383350a049
Sahil Sajan <sahilsajjan18@gmail.com>
2026-01-07 16:09:24 +0500

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Hero from "@/components/Home/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+    </>
   );
 }
