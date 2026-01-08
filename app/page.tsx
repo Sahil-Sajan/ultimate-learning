71ab14bd430234bca44e1b01ea18784816a015ca
Sahil Sajan <sahilsajjan18@gmail.com>
2026-01-08 11:38:35 +0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-import FeatureCards from "@/components/Home/CategoryGrid.";
+"use client";
+
+import CategoryGrid from "@/components/Home/CategoryGrid.";
+import Courses from "@/components/Home/Courses";
+// import FeatureCards from "@/components/Home/FeatureCards";
 import FeaturesSection from "@/components/Home/FeatureSection";
 import Hero from "@/components/Home/Hero";
+import InquirySection from "@/components/Home/InquirySection";
+import Testimonials from "@/components/Home/Testimonials";
+
+import Footer from "@/ui/Footer";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      {/* <FeatureCards /> */}
+      <FeaturesSection />
       <CategoryGrid />
+      <Courses />
+      <InquirySection />
+      <Testimonials />
+      <Footer />
     </div>
   );
 };
